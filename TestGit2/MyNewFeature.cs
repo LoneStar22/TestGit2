9f4b3fbff499e74e9adb8f21deb27fdaba40857a
Alexander Schmidt <AlexanderSchmidt01@web.de>
2012-08-01 16:32:14 +0200

    Added a new feature.

---
+++ TestGit2/MyNewFeature.cs
@@ -0,0 +1,18 @@
+// -------------------------------------------------------------------------------------------------
+//  <copyright file="${ITEMFILENAME}" company="AlfaSoft">
+//      AlfaSoft. All rights reserved.
+//  </copyright>
+//  <author>Alexander Schmidt</author>
+// -------------------------------------------------------------------------------------------------
+using System;
+
+namespace TestGit2
+{
+	public class MyNewFeature
+	{
+		public MyNewFeature()
+		{
+		}
+	}
+}
+
