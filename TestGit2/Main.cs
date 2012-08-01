9653c3ac5f79c3befe77f5a5f105f3f45b90039c
unknown-user <unknown-user@fe80::95a:9ca4:7cb0:43bb%34>
2012-08-01 16:05:39 +0200

    Erstes Einchecken des Moduls TestGit2

---
+++ TestGit2/Main.cs
@@ -0,0 +1,18 @@
+// -------------------------------------------------------------------------------------------------
+//  <copyright file="${ITEMFILENAME}" company="AlfaSoft">
+//      AlfaSoft. All rights reserved.
+//  </copyright>
+//  <author>Alexander Schmidt</author>
+// -------------------------------------------------------------------------------------------------
+using System;
+
+namespace TestGit2
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
